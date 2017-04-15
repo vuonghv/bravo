9e272897292c8bb10ab4d54b9a0ad113b717759f
Vuong Hoang <vuonghv.cs@gmail.com>
2017-04-16 02:02:38 +0700

    set-status-message

--- bravo.c
+++ bravo.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -389,6 +390,14 @@ void editor_refresh_screen() {
     ab_free(&ab);
 }
 
+void editor_set_status_message(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
+}
+
 /*** input ***/
 
 void editor_move_cursor(int key) {
@@ -500,6 +509,8 @@ int main(int argc, char *argv[]) {
     if (argc >= 2) {
         editor_open(argv[1]);
     }
+
+    editor_set_status_message("HELP: Ctrl-Q = quit");
 
     while (1) {
         editor_refresh_screen();
