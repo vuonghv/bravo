3246086438e7b176b0f5e634c599237c448ba74d
Vuong Hoang <vuonghv.cs@gmail.com>
2017-04-17 16:28:44 +0700

    search-arrows-help

--- bravo.c
+++ bravo.c
@@ -421,16 +421,35 @@ void editor_save() {
 /*** find ***/
 
 void editor_find_callback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
+
     if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
         return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
     }
 
+    if (last_match == -1) direction = 1;
+    int current = last_match;
     int i;
     for (i = 0; i < E.numrows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editor_row_rx_to_cx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
@@ -439,10 +458,21 @@ void editor_find_callback(char *query, int key) {
 }
 
 void editor_find() {
-    char *query = editor_prompt("Search: %s (ESC to cancel)", editor_find_callback);
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+    char *query = editor_prompt("Search: %s (Use ESC/Arrows/Enter)",
+                                editor_find_callback);
 
     if (query) {
         free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
     }
 }
 
