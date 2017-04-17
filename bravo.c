89f7edccb5ccb488c5e5d6f6a0d742a953aff875
Vuong Hoang <vuonghv.cs@gmail.com>
2017-04-17 11:33:08 +0700

    reset-dirty

--- bravo.c
+++ bravo.c
@@ -55,6 +55,7 @@ struct editor_config {
     int screencols;
     int numrows;
     erow *row;
+    int dirty;
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -242,6 +243,7 @@ void editor_append_row(char *s, size_t len) {
     editor_update_row(&E.row[at]);
 
     E.numrows++;
+    E.dirty++;
 }
 
 void editor_row_insert_char(erow *row, int at, int c) {
@@ -251,6 +253,7 @@ void editor_row_insert_char(erow *row, int at, int c) {
     row->size++;
     row->chars[at] = c;
     editor_update_row(row); // Update render, rsize
+    E.dirty++;
 }
 
 /*** editor operation ***/
@@ -302,6 +305,7 @@ void editor_open(char *filename) {
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editor_save() {
@@ -316,6 +320,7 @@ void editor_save() {
             if (write(fd, buf, len) == len) {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editor_set_status_message("%d bytes written to disk", len);
                 return;
             }
@@ -417,8 +422,9 @@ void editor_draw_status_bar(struct abuf *ab) {
     ab_append(ab, "\x1b[7m", 4); // black text on white background
     char status[80];
     char rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-            E.filename ? E.filename : "[No Name]", E.numrows);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+            E.filename ? E.filename : "[No Name]", E.numrows,
+            E.dirty ? "(modified)" : "");
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
     if (len > E.screencols) len = E.screencols;
     ab_append(ab, status, len);
@@ -593,6 +599,7 @@ void init_editor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
