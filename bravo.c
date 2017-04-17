3bd2b142db7d55daef508dd6937ebfc8d53405ec
Vuong Hoang <vuonghv.cs@gmail.com>
2017-04-17 14:37:49 +0700

    prompt-backspace

--- bravo.c
+++ bravo.c
@@ -68,6 +68,8 @@ struct editor_config E;
 /*** prototypes ***/
 
 void editor_set_status_message(const char *fmt, ...);
+void editor_refresh_screen();
+char *editor_prompt(char *prompt);
 
 /*** terminal ***/
 
@@ -374,7 +376,13 @@ void editor_open(char *filename) {
 }
 
 void editor_save() {
-    if (E.filename == NULL) return;
+    if (E.filename == NULL) {
+        E.filename = editor_prompt("Save as: %s");
+        if (E.filename == NULL) {
+            editor_set_status_message("Save aborted");
+            return;
+        }
+    }
 
     int len;
     char *buf = editor_rows_to_string(&len);
@@ -547,6 +555,40 @@ void editor_set_status_message(const char *fmt, ...) {
 }
 
 /*** input ***/
+
+char *editor_prompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editor_set_status_message(prompt, buf);
+        editor_refresh_screen();
+
+        int c = editor_read_key();
+        if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+            if (buflen != 0) buf[--buflen] = '\0';
+        } else if (c == '\x1b') {
+            editor_set_status_message("");
+            free(buf);
+            return NULL;
+        } else if (c == '\r') {
+            if (buflen != 0) {
+                editor_set_status_message("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editor_move_cursor(int key) {
     // Current row
